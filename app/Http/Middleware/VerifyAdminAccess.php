2806919033cbe3079d74d19e0c2a484adcd4b9e8
Jaime Sares <jaimesares@gmail.com>
2017-01-23 13:30:36 +0100

    Corregido un error de seguridad

--- app/Http/Middleware/VerifyAdminAccess.php
+++ app/Http/Middleware/VerifyAdminAccess.php
@@ -15,7 +15,10 @@ class VerifyAdminAccess
      */
     public function handle($request, Closure $next)
     {
-        if ($request->user() && ! $request->user()->hasPermission('admin')) {
+        if ($request->user() 
+            && ! $request->user()->hasPermission('admin') 
+            || $request->user()->isBanned()
+            || ! $request->user()->isAdminOrVolunteer()) {
             abort(401);
         }
 
