0cc75dabe7ab6014779078237cb0af049b8be2e0
Jaime Sares <jaimesares@gmail.com>
2017-07-04 01:17:15 +0200

    Corregido un error que impedía recuperar la cuenta

--- resources/views/emails/auth/recovery.blade.php
+++ resources/views/emails/auth/recovery.blade.php
@@ -14,7 +14,7 @@
             <td width="200" height="36" bgcolor="#25c2e6" align="center" valign="middle"
                 style="font-family: Arial,Helvetica,sans-serif; font-size: 16px; color: #ffffff;
         line-height:18px;">
-                <a href="{{ route('auth::password', ['token' => $user->remember_token]) }}" target="_blank" alias="" style="text-decoration: none; color: #ffffff;">Recuperar cuenta</a>
+                <a href="http://{{ $user->web->getDomainOrSubdomain() }}{{ route('auth::password', ['token' => $user->remember_token], false) }}" target="_blank" alias="" style="text-decoration: none; color: #ffffff;">Recuperar cuenta</a>
             </td>
         </tr>
     </table>
